6168b9b87eaf0a6b5a65623a4fe952c71ce2727b
F4brice74 <fabrice.event.manager@gmail.com>
2021-03-29 10:46:23 +0200

    disable order button when scoop is zero

--- src/pages/entry/OrderEntry.js
+++ src/pages/entry/OrderEntry.js
@@ -1,12 +1,16 @@
 import Options from './Options';
 import { useOrderDetails} from '../../context/OrderDetails'
 import Button from 'react-bootstrap/Button'
+import { useState, useEffect} from 'react';
 
 
 export default function OrderEntry({setOrderPhase}) {
     
     const [orderDetails] = useOrderDetails();
-   
+  
+    let buttonEnable = orderDetails.totals.scoops === '$0.00'
+    
+   //console.log(orderDetails.totals.scoops)
     return (
        
         <div>
@@ -14,7 +18,7 @@ export default function OrderEntry({setOrderPhase}) {
             <Options optionType='toppings' />
             <h2>Grand Total: {orderDetails.totals.grandTotal} </h2>
         
-            <Button variant="primary" onClick={() => setOrderPhase('review')}>Order Sundae !</Button>
+            <Button disabled={buttonEnable} variant="primary" onClick={() => setOrderPhase('review')}>Order Sundae !</Button>
         </div>
     )
 }
--- src/pages/entry/tests/OrderEntry.test.js
+++ src/pages/entry/tests/OrderEntry.test.js
@@ -1,19 +1,55 @@
-import { render, screen, waitFor } from '../../../test-utils/testing-library-utils';
-import OrderEntry from '../OrderEntry';
-import { rest } from 'msw';
-import { server } from '../../../mocks/server';
+import {
+  render,
+  screen,
+  waitFor,
+} from "../../../test-utils/testing-library-utils";
+import OrderEntry from "../OrderEntry";
+import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
 
-
-test('handles error for scoops and toppings routes', async () => {
-    // reset handler
-    server.resetHandlers(
-        rest.get('http://localhost:3030/scoops', (req, res, ctx) => res(ctx.status(500))),
-        rest.get('http://localhost:3030/toppings', (req, res, ctx) => res(ctx.status(500)))
+test("handles error for scoops and toppings routes", async () => {
+  // reset handler
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
+      res(ctx.status(500))
+    ),
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
+      res(ctx.status(500))
     )
-    render(<OrderEntry setOrderPhase={jest.fn()}/>);
-    await waitFor(async () => {
-        const alerts = await screen.findAllByRole('alert');
-        expect(alerts).toHaveLength(2);
-    })
+  );
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
+  });
+});
 
-})
+test("disable Order button for no scoops", async () => {
+  render(<OrderEntry />);
+  // define Scoops
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  // define OrderButton
+  const orderButton = screen.getByRole("button", { name: "Order Sundae !" });
+  //set scoop to zero
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "0");
+  // check if button is disabled
+  expect(orderButton).toBeDisabled();
+
+  // set scoop to one
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  //check if button is enabled
+  expect(orderButton).toBeEnabled();
+
+  // set scoop to zero
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "0");
+  // check if button is disabled
+  expect(orderButton).toBeDisabled();
+
+});
